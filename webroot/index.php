ef6f7721f006039eeb4df12f941b4e7b0199a45e
Simen Lybekk <simen@lybekk.no>
2018-02-05 14:33:54 +0100

    Add boilerplate

---
+++ webroot/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once dirname(dirname(__FILE__)) . "/lib.php";
+
+$loader = new Twig_Loader_Array(array(
+    'index' => 'Hello {{ name }}!',
+));
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index', array('name' => 'Fabien'));
