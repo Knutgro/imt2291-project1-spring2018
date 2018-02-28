cbd8c6c4efb8c3639593930cc6377fab19d8067c
Simen Lybekk <simen@lybekk.no>
2018-02-28 18:54:07 +0100

    Add ratings

--- webroot/watch.php
+++ webroot/watch.php
@@ -16,7 +16,7 @@ if (!$video) {
 $error = "";
 if ($user && !empty($_POST)) {
 
-    // Comments
+    // Comment
     if ( $_POST["action"] == "comment" && !empty(trim($_POST["comment"])) )
     {
         $comment = new Comment( $user->getId(), $video->getId(), $_POST["comment"] );
@@ -27,12 +27,29 @@ if ($user && !empty($_POST)) {
             $error = "Unable to post comment";
         }
     }
+
+    // Rating
+    if ( $_POST["action"] == "rate" && array_key_exists("rating", $_POST)
+        && $_POST["rating"] >= 0 && $_POST["rating"] <= 5 )
+    {
+        $rating = new Rating( $user->getId(), $video->getId(), $_POST["rating"] );
+        if ( $rating->insert() !== false) {
+            header("Location: ${_SERVER["REQUEST_URI"]}");
+            die();
+        } else {
+            $error = "Unable to submit rating";
+        }
+    }
 }
 
 
 echo $twig->render('watch.twig', [
+    "user"     => $user,
+
     "video"    => $video,
     "comments" => Comment::getCommentsByVideoId($video->getId()),
-    "user"     => $user,
+    "rating"   => Rating::getTotalRating($video->getId()),
+    "myrating" => Rating::getUserRating($user->getId(), $video->getId()),
+
     "error"    => $error,
 ]);
