4bad18518ed5836f787a98d3296e76ad515859cc
Knut <knutgro@stud.ntnu.no>
2018-03-01 17:27:38 +0100

    Myplaylists added

---
+++ webroot/myPlaylists.php
@@ -0,0 +1,26 @@
+<?php
+require_once dirname(dirname(__FILE__)) . "/lib.php";
+
+$error = "";
+$msg = "";
+$user = User::getById(1);
+//$user = User::loggedIn();
+
+
+// Redirect the user away if they aren't logged in or doesn't have admin privileges
+if (is_null($user) || !$user->is(User::ADMIN || $user->is(User::LECTURER))) {
+    header("Location: /");
+    die();
+}
+
+$playlists = Playlist::getPlaylistByUser($user->getId());
+
+
+
+
+echo $twig->render('myPlaylists.twig', [
+    "playlists" => $playlists,
+    "userId" => $user->getId(),
+    "user" => User::loggedIn()
+
+]);
