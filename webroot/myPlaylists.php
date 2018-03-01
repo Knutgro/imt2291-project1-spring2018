aef5727d0ae9b7cb28087cdce7c1f7a525e9434e
Simen Lybekk <simen@lybekk.no>
2018-03-01 19:46:53 +0100

    Add videos to the playlist page

--- webroot/myPlaylists.php
+++ webroot/myPlaylists.php
@@ -9,30 +9,15 @@ $user = User::loggedIn();
 
 
 // Redirect the user away if they aren't logged in or doesn't have admin privileges
-if (is_null($user) || !$user->is(User::ADMIN || $user->is(User::LECTURER))) {
+if (is_null($user) || !$user->isLecturer()) {
     header("Location: /");
     die();
 }
 
-//Stores the playlists of the given user.
-$playlists = Playlist::getPlaylistByUser($user->getId());
-
-if (!empty($_POST)) {
-
-    if (empty($_POST["video"])) {
-        $errors[] = "You must select at least one video";
-    }
-
-    if (empty($errors)) {
-        $_SESSION['playlist'] = $_POST["playlist"];
-    }
-}
-
-
 
 echo $twig->render('myPlaylists.twig', [
-    "playlists" => $playlists,
-    "userId" => $user->getId(),
-    "user" => User::loggedIn()
+    "user" => $user,
 
+    "playlists" => Playlist::getPlaylistByUser($user->getId()),
+    "videos"    => Video::getByUser($user->getId()),
 ]);
