1e41639349aa5c7c34b0e0133302068d507a57a1
Simen Lybekk <simen@lybekk.no>
2018-03-02 02:42:26 +0100

    Make PHPunit tests for User data-independent

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -9,18 +9,27 @@ use PHPUnit\Framework\TestCase;
 
 final class UserTest extends TestCase
 {
+    /**
+     * Prepare database transaction
+     */
     public function setUp()
     {
         $dbh = DB::getPDO();
         $dbh->beginTransaction();
     }
 
+    /**
+     * DB cleanup
+     */
     public function tearDown()
     {
         $dbh = DB::getPDO();
         $dbh->rollBack();
     }
 
+    /**
+     * Verify that password functions work
+     */
     public function testPasswordSetVerify()
     {
         $password = "testPasswordForTesting";
@@ -37,12 +46,18 @@ final class UserTest extends TestCase
         $this->assertFalse($user->verifyPassword($password));
     }
 
+    /**
+     * Verify that get email works
+     */
     public function testGetEmail()
     {
         $user = new User("my@email.no");
         $this->assertEquals("my@email.no", $user->getEmail());
     }
 
+    /**
+     * Verify that get type works
+     */
     public function testType()
     {
         $user = new User(null, null, "student");
@@ -61,68 +76,34 @@ final class UserTest extends TestCase
         $this->assertEquals("student", $user->getType());
     }
 
-    public function testGetLoggedInUser()
-    {
-        unset($_SESSION["user_id"]);
-
-        $this->assertNull(User::loggedIn());
-
-        $_SESSION["user_id"] = 1;  // The admin user should have ID 1 in the DB
-
-        $this->assertInstanceOf(User::class, User::loggedIn());
-    }
-
+    /**
+     * Verify that we can look up users by ID
+     */
     public function testGetUserById()
     {
-        $user = User::getById(1);  // The admin user should have ID 1 in the DB
+        // Insert a video into the DB, avoiding using the insert method as
+        // its tests depends on us.
+        $dbh = DB::getPDO();
+        $stmt = $dbh->prepare(
+            "INSERT INTO user (email, password, type) "
+          . "VALUES ('test@user.nosteal', 'lmao', 'student');"
+        );
+        $this->assertTrue($stmt->execute());
+
+        // Get the inserted ID form the DB
+        $this->id = $dbh->lastInsertId();
+
+        $user = User::getById($this->id);
 
         $this->assertInstanceOf(User::class, $user);
-        $this->assertEquals(1, $user->getId());
+        $this->assertEquals($this->id, $user->getId());
 
         $this->assertNull(User::getById(-1));
     }
 
-    public function testGetUserByEmail()
-    {
-        $email = "video-admin@ntnu.no";
-        $user = User::getByEmail($email);  // The admin user should have ID 1 in the DB
-
-        $this->assertInstanceOf(User::class, $user);
-        $this->assertEquals($email, $user->getEmail());
-
-        $this->assertNull(User::getByEmail("invalid"));
-    }
-
-    public function testDoLogin()
-    {
-        // Invalid email
-        $this->assertFalse(User::doLogin("nonexistant", "lol"));
-
-        // Invalid password
-        $this->assertFalse(User::doLogin("video-admin@ntnu.no", "lol"));
-
-        // Valid everything
-        $this->assertTrue(User::doLogin("video-admin@ntnu.no",
-            "do not use in production"));
-    }
-
-    public function testValidate()
-    {
-        $errors = User::validate("asdf", "aaa", "bbb", "admin");
-
-        $this->assertContains("Invalid email", $errors);
-        $this->assertContains("Password should be stronger", $errors);
-        $this->assertContains("Passwords do not match", $errors);
-        $this->assertContains("Invalid user type", $errors);
-
-        $errors = User::validate("video-admin@ntnu.no", "a23456789", "a23456789",
-            "lecturer");
-        $this->assertContains("Email already exists", $errors);
-
-        $errors = User::validate("test@email.no", "a23456789", "a23456789", "student");
-        $this->assertEmpty($errors);
-    }
-
+    /**
+     * Verify that we can verify users
+     */
     public function testVerifiedFlag()
     {
         $user = new User();
@@ -136,58 +117,13 @@ final class UserTest extends TestCase
         $this->assertFalse($user->isVerified());
     }
 
-    public function testAccessLevel()
-    {
-        // Verify student levels
-        $user = new User(null, null, "student");
-
-        $user->setVerified(false);
-        $this->assertEquals($user->getAccessLevel(), User::STUDENT);
-        $user->setVerified(true);
-        $this->assertEquals($user->getAccessLevel(), User::STUDENT);
-
-        // Verify student levels
-        $user = new User(null, null, "lecturer");
-
-        $user->setVerified(false);
-        $this->assertEquals($user->getAccessLevel(), User::STUDENT);
-        $user->setVerified(true);
-        $this->assertEquals($user->getAccessLevel(), User::LECTURER);
-
-        // Verify student levels
-        $user = new User(null, null, "admin");
-
-        $user->setVerified(false);
-        $this->assertEquals($user->getAccessLevel(), User::STUDENT);
-        $user->setVerified(true);
-        $this->assertEquals($user->getAccessLevel(), User::ADMIN);
-    }
-
-    public function testHasAccess()
-    {
-        $user = new User(null, null, "admin");
-
-        $this->assertTrue($user->is(User::STUDENT));
-        $this->assertTrue($user->isStudent());
-
-        $this->assertFalse($user->is(User::LECTURER));
-        $this->assertFalse($user->isLecturer());
-
-        $this->assertFalse($user->is(User::ADMIN));
-        $this->assertFalse($user->isAdmin());
-
-        $user->setVerified(true);
-
-        $this->assertTrue($user->is(User::STUDENT));
-        $this->assertTrue($user->isStudent());
-
-        $this->assertTrue($user->is(User::LECTURER));
-        $this->assertTrue($user->isLecturer());
-
-        $this->assertTrue($user->is(User::ADMIN));
-        $this->assertTrue($user->isLecturer());
-    }
-
+    /**
+     * Verify that we can insert and update data in the database
+     *
+     * @depends testGetUserById
+     * @depends testType
+     * @depends testVerifiedFlag
+     */
     public function testInsertUpdate()
     {
         // Prepare a basic user and insert it into the database
@@ -221,6 +157,148 @@ final class UserTest extends TestCase
         // Verify that the fields got changed
         $this->assertTrue($fetchedUser->isVerified());
         $this->assertEquals("admin", $fetchedUser->getType());
+    }
+
+    /**
+     * Verify that we can get the logged in user from the current session
+     *
+     * @depends testInsertUpdate
+     */
+    public function testGetLoggedInUser()
+    {
+        $user = new User("mock@pls2not.disturb", "asdf", "student");
+        $user->insert();
+
+        unset($_SESSION["user_id"]);
+
+        $this->assertNull(User::loggedIn());
+
+        $_SESSION["user_id"] = $user->getId();
+
+        $this->assertInstanceOf(User::class, User::loggedIn());
+    }
+
+    /**
+     * Verify that we can look up users by email
+     *
+     * @depends testInsertUpdate
+     */
+    public function testGetUserByEmail()
+    {
+        $mockUser = new User("mock@pls2not.disturb", "asdf", "student");
+        $mockUser->insert();
+
+        $user = User::getByEmail($mockUser->getEmail());
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($mockUser->getEmail(), $user->getEmail());
+
+        $this->assertNull(User::getByEmail("invalid"));
+    }
+
+    /**
+     * Verify that users can log in to their accounts
+     *
+     * @depends testGetLoggedInUser
+     * @depends testInsertUpdate
+     * @depends testGetUserByEmail
+     */
+    public function testDoLogin()
+    {
+        $mockUser = new User("mock@pls2not.disturb", "asdf", "student");
+        $mockUser->insert();
+
+        // Invalid email
+        $this->assertFalse(User::doLogin("nonexistant", "lol"));
+
+        // Invalid password
+        $this->assertFalse(User::doLogin("mock@pls2not.disturb", "lol"));
+
+        // Valid everything
+        $this->assertTrue(User::doLogin("mock@pls2not.disturb", "asdf"));
+    }
+
+    /**
+     * Verify that user registration validation works
+     *
+     * @depends testInsertUpdate
+     */
+    public function testValidate()
+    {
+        $user = new User("test-existing@donot.steal", "asdfasdf", "student");
+        $this->assertNotFalse($user->insert());
+
+        $errors = User::validate("asdf", "aaa", "bbb", "admin");
+
+        $this->assertContains("Invalid email", $errors);
+        $this->assertContains("Password should be stronger", $errors);
+        $this->assertContains("Passwords do not match", $errors);
+        $this->assertContains("Invalid user type", $errors);
+
+        $errors = User::validate($user->getEmail(), "a23456789", "a23456789",
+            "lecturer");
+        $this->assertContains("Email already exists", $errors);
+
+        $errors = User::validate("test@email.no", "a23456789", "a23456789", "student");
+        $this->assertEmpty($errors);
+    }
+
+    /**
+     * Verify that the user only gets higher acess levels if they're verified
+     */
+    public function testAccessLevel()
+    {
+        // Verify student levels
+        $user = new User(null, null, "student");
+
+        $user->setVerified(false);
+        $this->assertEquals($user->getAccessLevel(), User::STUDENT);
+        $user->setVerified(true);
+        $this->assertEquals($user->getAccessLevel(), User::STUDENT);
+
+        // Verify student levels
+        $user = new User(null, null, "lecturer");
+
+        $user->setVerified(false);
+        $this->assertEquals($user->getAccessLevel(), User::STUDENT);
+        $user->setVerified(true);
+        $this->assertEquals($user->getAccessLevel(), User::LECTURER);
+
+        // Verify student levels
+        $user = new User(null, null, "admin");
+
+        $user->setVerified(false);
+        $this->assertEquals($user->getAccessLevel(), User::STUDENT);
+        $user->setVerified(true);
+        $this->assertEquals($user->getAccessLevel(), User::ADMIN);
+    }
+
+    /**
+     * Verify that access privilege checking works properly
+     */
+    public function testHasAccess()
+    {
+        $user = new User(null, null, "admin");
+
+        $this->assertTrue($user->is(User::STUDENT));
+        $this->assertTrue($user->isStudent());
+
+        $this->assertFalse($user->is(User::LECTURER));
+        $this->assertFalse($user->isLecturer());
+
+        $this->assertFalse($user->is(User::ADMIN));
+        $this->assertFalse($user->isAdmin());
+
+        $user->setVerified(true);
+
+        $this->assertTrue($user->is(User::STUDENT));
+        $this->assertTrue($user->isStudent());
+
+        $this->assertTrue($user->is(User::LECTURER));
+        $this->assertTrue($user->isLecturer());
+
+        $this->assertTrue($user->is(User::ADMIN));
+        $this->assertTrue($user->isLecturer());
     }
 
 }
